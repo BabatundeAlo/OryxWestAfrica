3537e57bb3e0fa4a4082d08a8b07ab603bd1d81b
BabatundeAlo <babatunde.olabode@oryx-wa.com>
2018-08-06 17:05:55 +0800

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OryxWestAfrica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
